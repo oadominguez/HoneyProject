94b2b4beb64d8fb949448e383f5fae034040a6c2
rcarmona <andres_zlatan@hotmail.com>
2014-07-03 14:45:03 -0500

    Agregando servicios
    Servicios agregados con tarpiting

--- HoneyProject.pl
+++ HoneyProject.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 use IO::Socket::INET;
+use Time::HiRes qw(gettimeofday usleep);
 use threads;
 
 sub ipParser
@@ -76,7 +77,7 @@ sub chargenServer
 		$chargenSocket->recv($dataRcv,2048);
 		$clientAddr = $chargenSocket->peerhost();
 		$clientPort = $chargenSocket->peerport();
-		l
+		
 		$peticiones{$clientAddr}++; #Aumenta peticiones por cada ip;
 
 
@@ -245,7 +246,7 @@ sub dnsLogic
   my $response = $ID.$flags.$numPreg.$numResp.$numRespAuth.$numRespAddit.$dataQuery;
   my $pointer = "c00c";
   my $tipoRes = $type;
-  my $classRes = $class;l
+  my $classRes = $class;
   my $ttl = "00000007"; #Segundos de vida
   $response = $response.$pointer.$tipoRes.$classRes.$ttl.$tamRes.$dataRes;
   return $response;
@@ -264,7 +265,7 @@ sub dnsServer
 	my %peticiones=();
 
 	##Lectura del archivo de configuración para obtener tarpiting
-	open(CONFIG,"dns.conf") or die "No se pudo abrir";
+	open(CONFIG,"ntp.conf") or die "No se pudo abrir";
 	  while(<CONFIG>){
 			chomp($_);
 			if ($_ =~ m/^Tarpiting.*/g){
@@ -315,6 +316,337 @@ sub dnsServer
 	$dnsSocket->close();
 }
 
+
+sub ntpLogic{
+
+		my $ntpIP=shift;
+		my $dataRcv=shift;
+		#chop($ntpIP);
+		#chop($dataRcv);
+		#print "$ntpIP\n$dataRcv\n";
+
+		my $ntpRespond="";
+		my ($ReceiveSec, $ReceiveF) = gettimeofday();
+		$ReceiveSec+=2208988800; #Equivalente en segundos de 70 años
+		$ReceiveSec = sprintf("%x",$ReceiveSec); #Conversion a hexadecimal de 4 bytes
+		$ReceiveF = sprintf("%08x",$ReceiveF); #Conversion a hexadecimal de 4 bytes agregando bytes faltantes
+
+		my $dataHex = $dataRcv;
+		$dataHex =~ s/(.)/sprintf("%02x",ord($1))/eg;
+		#print "dataHex $dataHex\n";
+		my @hexData=($dataHex =~ m/../g ); #Arreglo de bytes
+		#print "@hexData\n";
+		#Armando paquete de respuesta
+		$ntpRespond.="24"; #LI - No warning; Version NTP4; Mode - Server
+		#$ntpRespond.="1C"; #LI - No warning; Version NTP4; Mode - Server
+		$ntpRespond.="02"; #Primary Interface
+		$ntpRespond.= $hexData[2]; # Poll Interval
+		$ntpRespond.="fa"; # Clock Precision
+		$ntpRespond.="00010000"; #Root Delay - 1 segundo
+		$ntpRespond.="000005cb"; #Root Dispersion 0.0226 segundos
+
+		#Reference ID (IP servidor)
+		my @ip = split('\.',$ntpIP);
+		for (my $i=0;$i<4;$i++){
+			$ntpRespond.=sprintf("%02x",$ip[$i]);
+		}
+		
+
+		#Reference Timestamp (Hora actual del servidor)
+		my ($ReferSec, $ReferF) = gettimeofday();
+		$ReferSec+=2208988800; #Equivalente en segundos de 70 años
+		$ReferSec = sprintf("%x",$ReferSec); #Conversion a hexadecimal de 4 bytes
+		$ReferF = sprintf("%08x",$ReferF);
+		#print "Reference: $ReferSec $ReferF\n";
+
+		$ntpRespond.="$ReferSec";
+		$ntpRespond.="$ReferF";
+
+		#Originate Time Stamp proveniente del campo Transmit Timestamp del paquete recibido (Hora a la que el cliente envio el paquete)
+		for (my $i=40;$i<48;$i++){
+			$ntpRespond.=$hexData[$i];
+		}
+
+		#Receive Time Stamp (Hora a la que se recibio el paquete del cliente)
+		#print "Receive: $ReceiveSec $ReceiveF\n";
+		$ntpRespond.="$ReceiveSec";
+		$ntpRespond.="$ReceiveF";
+
+		#Transmit Timestamp (Hora a la que se envia el paquete)
+		my ($TransSec, $TransF) = gettimeofday();
+		$TransSec+=2208988800; #Equivalente en segundos de 70 años
+		$TransSec = sprintf("%x",$TransSec); #Conversion a hexadecimal de 4 bytes
+		$TransF = sprintf("%08x",$TransF);
+		#print "Transmit: $TransSec $TransF\n";
+		$ntpRespond.="$TransSec";
+		$ntpRespond.="$TransF";
+		return $ntpRespond;
+
+}
+
+sub ntpServer{
+	my $ntpIP=$_[0];
+	my $ntpPort=123;
+	my $ntpSocket;
+	my $clientAddr;
+	my @times;
+	my %peticiones=();
+	print "ntpIP $ntpIP\n";
+	##Lectura del archivo de configuración para obtener tarpiting
+	open(CONFIG,"ntp.conf") or die "No se pudo abrir";
+	  while(<CONFIG>){
+			chomp($_);
+			if ($_ =~ m/^Tarpiting.*/g){
+				@times=split(' ',$_);
+			}
+		}
+	close(CONFIG);
+
+		print "Creando UDP Socket...[+] \n";
+	# Creamos el Socket
+	$ntpSocket = IO::Socket::INET->new(LocalPort => $ntpPort,LocalAddr =>$ntpIP,Type => SOCK_DGRAM,Proto => 'udp') or die "No se pudo crear el socket UDP $! \n";
+
+
+	print "UDP Socket Creado...[+] \n";
+	print "Servidor a la escucha en UDP bajo puerto $ntpPort\n";
+	while(1)
+	{	
+		my $ntpRespond="";
+		my $dataRcv ="";
+		$ntpSocket->recv($dataRcv,2048);
+		#print "rec: $dataRcv\n";
+		$ntpRespond=ntpLogic($ntpIP,$dataRcv);
+		$clientAddr = $ntpSocket->peerhost();
+		$peticiones{$clientAddr}++; #Aumenta peticiones por cada ip;
+
+		my $contador=$peticiones{$clientAddr}-1;
+		if ($contador==0){
+			print "Enviando respuesta NTP...\n\n";
+			$ntpSocket->send(pack("H*",$ntpRespond));
+		}
+		elsif($contador<10){
+			print "Enviando respuesta NTP después de $times[$contador] microsegundos...\n\n";
+			usleep($times[$contador]);
+			$ntpSocket->send(pack("H*",$ntpRespond));
+		}
+		else{
+			print "Enviando respuesta NTP después de $times[10] microsegundos...\n\n";
+			usleep($times[10]);
+			$ntpSocket->send(pack("H*",$ntpRespond));
+		}
+		
+	}
+	$ntpSocket->close();
+
+}
+
+
+sub netbiosLogic{
+	my $dataHex=shift;
+	my $equipo=shift;
+	#Leer archivo de configuración
+	chop($equipo);
+	#print "Equipo: $equipo\n";
+	#Modificación del original para convertirlo en un paquete de respuesta
+	my $i=0;
+	my @hexData=($dataHex =~ m/../g ); #Arreglo de bytes
+	my $tmp='';
+	foreach my $byte (@hexData){
+		#Name Query Response, no error
+		if ($i==2){$tmp.="84";} 
+
+		elsif ($i==3){$tmp.="00";}
+		#Questions
+		elsif ($i==4){$tmp.="00";}
+		elsif ($i==5){$tmp.="00";}
+		#Answer
+		elsif ($i==6){$tmp.="00";}
+		elsif ($i==7){$tmp.="01";}
+		else{$tmp.=$byte;}
+		$i++;
+	}
+	$tmp.="00000000";# TTL
+	$tmp.="009b06";# Data Length and Number of names
+
+	#Nombre del equipo falso (hasta 16 caracteres)
+	$equipo=~ s/(.)/sprintf("%02x",ord($1))/eg;
+	my $t1=(length($equipo)/2);
+
+	#Se llena con 20 los caracteres faltantes
+	for (my $i=0;$i<15-$t1;$i++){
+		$equipo.="20";
+	}
+	$tmp.=$equipo;# NombreEquipo
+	$tmp.="004400574f524b47524f555020202020202000c400"; #Datos adicionales, Workgroup
+	$tmp.=$equipo;# NombreEquipo
+	#Resto del paquete, incluye worgroup y MAC Address
+	$tmp.="204400574f524b47524f55502020202020201ec400574f524b47524f55502020202020201d440001025f5f4d5342524f5753455f5f0201c400000c2924709200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";	
+	return $tmp;
+
+}
+
+sub netbiosServer{
+	my $nbIP=$_[0];
+	my $nbPort=137;
+	my $nbSocket;
+	my $clientAddr;
+	my $clientPort;
+	my @times;
+	my %peticiones=();
+	my $nombreEquipo;
+
+	##Lectura del archivo de configuración para obtener nombre de equipo y tarpiting
+	open(CONFIG,"netbios.conf") or die "No se pudo abrir";
+	  while(<CONFIG>){
+			chomp($_);
+			if ($_ =~ m/^Equipo (.*)/g){
+				$nombreEquipo=$1;
+			}
+			if ($_ =~ m/^Tarpiting.*/g){
+				@times=split(' ',$_);
+			}
+		}
+	close(CONFIG);
+	#print "$nombreEquipo\n@times\n";
+	print "Creando UDP Socket...[+] \n";
+	# Creamos el Socket
+	$nbSocket = IO::Socket::INET->new(LocalPort => $nbPort,
+																		Type => SOCK_DGRAM,
+																		LocalAddr => $nbIP,
+																		Proto => 'udp') or die "No se pudo crear el socket UDP $! \n";
+	print "UDP Socket Creado...[+] \n";
+	print "Servidor a la escucha en UDP bajo puerto $nbPort\n";
+
+	while(1)
+	{
+		my $dataRcv ="";
+		$nbSocket->recv($dataRcv,2048);
+		my $dataHex = $dataRcv;
+		$dataHex =~ s/(.)/sprintf("%02x",ord($1))/eg;
+		$clientAddr = $nbSocket->peerhost();
+
+		$peticiones{$clientAddr}++; #Aumenta peticiones por cada ip;
+
+
+		my $netbiosData=netbiosLogic($dataHex,$nombreEquipo);
+		if ($peticiones{$clientAddr}==0){
+			print "Enviando respuesta...\n\n";
+			$nbSocket->send(pack("H*",$netbiosData));
+		}
+		elsif($peticiones{$clientAddr}<10){
+			print "Enviando respuesta despues de $times[$peticiones{$clientAddr}] microsegundos...\n\n";
+			usleep($times[$peticiones{$clientAddr}]);
+			$nbSocket->send(pack("H*",$netbiosData));
+		}
+		else{
+			print "Enviando respuesta despues de $times[10] microsegundos...\n\n";
+			usleep($times[10]);
+			$nbSocket->send(pack("H*",$netbiosData));
+		}
+
+	}
+	$nbSocket->close();
+}
+
+
+sub ssdpLogic{
+
+	my $location;
+	my $server;
+	my $st;
+	my $usn;
+
+	##Lectura del archivo de configuración para obtener tarpiting
+	open(CONFIG,"ssdp.conf") or die "No se pudo abrir";
+	  while(<CONFIG>){
+			chomp($_);
+			if ($_ =~ m/^LOCATION (.*)/g){
+				$location=$1;
+			}
+			if ($_ =~ m/^SERVER (.*)/g){
+				$server=$1;
+			}
+			if ($_ =~ m/^ST (.*)/g){
+				$st=$1;
+			}
+			if ($_ =~ m/^USN (.*)/g){
+				$usn=$1;
+			}
+		}
+	close(CONFIG);
+
+	my $ssdpData = "HTTP/1.1 200 OK
+CACHE-CONTROL: max-age=172800
+DATE: ".gmtime()." GMT
+EXT:
+LOCATION: $location
+SERVER: $server
+ST: $st
+USN: $usn\n\n";
+	#print "$ssdpData\n";
+	return $ssdpData;
+}
+
+
+sub ssdpServer{
+	my $ssdpPort=1900;
+	my $ssdpSocket;
+	my $clientAddr;
+	my $ssdpIP=$_[0];
+	my $ssdpData;
+	my @times;
+	my %peticiones=();
+
+	##Lectura del archivo de configuración para obtener tarpiting
+	open(CONFIG,"ssdp.conf") or die "No se pudo abrir";
+	  while(<CONFIG>){
+			chomp($_);
+			if ($_ =~ m/^Tarpiting.*/g){
+				@times=split(' ',$_);
+			}
+		}
+	close(CONFIG);
+
+
+	print "Creando UDP Socket...[+] \n";
+	# Creamos el Socket
+	$ssdpSocket = IO::Socket::INET->new(LocalPort => $ssdpPort,
+																			Type => SOCK_DGRAM,
+																			LocalAddr => $ssdpIP,
+																			Proto => 'udp') or die "No se pudo crear el socket UDP $! \n";
+	print "UDP Socket Creado...[+] \n";
+	print "Servidor a la escucha en UDP bajo puerto $ssdpPort\n";
+	while(1)
+	{
+		my $dataRcv ="";
+		$ssdpSocket->recv($dataRcv,2048);
+		my $dataHex = $dataRcv;
+		$dataHex =~ s/(.)/sprintf("%02x",ord($1))/eg;
+		$clientAddr = $ssdpSocket->peerhost();
+		$peticiones{$clientAddr}++; #Aumenta peticiones por cada ip;
+
+		$ssdpData=ssdpLogic();
+		my $contador=$peticiones{$clientAddr}-1;
+		if ($contador==0){
+			print "Enviando respuesta SSDP...\n\n";
+			$ssdpSocket->send($ssdpData);
+		}
+		elsif($contador<10){
+			print "Enviando respuesta SSDP después de $times[$contador] microsegundos...\n\n";
+			usleep($times[$contador]);
+						$ssdpSocket->send($ssdpData);
+		}
+		else{
+			print "Enviando respuesta SSDP después de $times[10] microsegundos...\n\n";
+			usleep($times[10]);
+						$ssdpSocket->send($ssdpData);
+		}
+	}
+	$ssdpSocket->close();
+}
+
+
+
 my @threads;
 print "Iniciando servidores CHARGEN\n";
 my @chargenIp = ipParser("chargen.conf");
@@ -352,4 +684,44 @@ if(@dnsIp)
 		push(@threads,$hilo);
 	}
 }
+
+print "Iniciando servidores NTP \n";
+my @ntpIp = ipParser("ntp.conf");
+if(@ntpIp)
+{
+	shift(@ntpIp);
+	print "Servicio NTP iniciara en las ip: @ntpIp \n";
+	foreach(@ntpIp)
+	{
+		my $hilo=threads->new(\&ntpServer,$_);
+		push(@threads,$hilo);
+	}
+}
+
+print "Iniciando servidores NETBIOS \n";
+my @netbiosIp = ipParser("netbios.conf");
+if(@netbiosIp)
+{
+	shift(@netbiosIp);
+	print "Servicio NETBIOS iniciara en las ip: @netbiosIp \n";
+	foreach(@netbiosIp)
+	{
+		my $hilo=threads->new(\&netbiosServer,$_);
+		push(@threads,$hilo);
+	}
+}
+
+print "Iniciando servidores SSDP \n";
+my @ssdpIp = ipParser("ssdp.conf");
+if(@ssdpIp)
+{
+	shift(@ssdpIp);
+	print "Servicio SSDP iniciara en las ip: @ssdpIp \n";
+	foreach(@ssdpIp)
+	{
+		my $hilo=threads->new(\&ssdpServer,$_);
+		push(@threads,$hilo);
+	}
+}
+
 foreach (@threads) { my $id = $_->join; }
